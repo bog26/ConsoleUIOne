c78c133d3740940411132222fbf121eb7c2083d8
bog26 <l_bogdan2001@yahoo.com>
2021-08-18 00:39:57 +0200

    Update IApp.cs

--- IApp.cs
+++ IApp.cs
@@ -1,22 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ShellMenuNS
 {
-	public interface IFrameItem
+	public interface IApp<T,U>:IMenu<T,U>
     {
-		int FrameItemNr {get;set;}
-		string TextDisplay {get;set;}
-		int Column {get;set;}
-		int Row {get;set;}
-		int[] GridPosition {get;set;}
-		bool IsCrtDisplayItem {get;set;}
-		bool IsDynamic {get;set;}
-		bool IsActionTrigger {get;}
-		int Link {get;}
-		bool ReadyForUpdate {get;set;}
-		//Dictionary<int,IFrameItem> ConstructSortedItemsDict();
-		Dictionary<int,IFrameItem> ReadItemsFile();
-		//Dictionary<int,IFrameItem> SortItems(Dictionary<int,IFrameItem> unsortedDict);
+		int AppID {get;}
+		string AppTextDisplay {get;}
+		void ConstructMenu();
+		void AppMethods(int select, Func<int> MethodDelegate);
 
 
     }
